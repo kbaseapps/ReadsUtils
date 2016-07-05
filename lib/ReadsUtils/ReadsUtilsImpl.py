ce24d6d47a7b64850b8be5601d2fa34ed4238905
Gavin Price <gaprice@lbl.gov>
2016-07-05 16:12:05 -0700

    post make commit

---
+++ lib/ReadsUtils/ReadsUtilsImpl.py
@@ -0,0 +1,40 @@
+#BEGIN_HEADER
+#END_HEADER
+
+
+class ReadsUtils:
+    '''
+    Module Name:
+    ReadsUtils
+
+    Module Description:
+    A KBase module: ReadsUtils
+    '''
+
+    ######## WARNING FOR GEVENT USERS #######
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    #########################################
+    VERSION = "0.0.1"
+    GIT_URL = "https://github.com/mrcreosote/ReadsUtils"
+    GIT_COMMIT_HASH = "b92a5a7e1fa9a392c3397c56f8f62ccd99136473"
+    
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        #END_CONSTRUCTOR
+        pass
+    
+
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK", 'message': "", 'version': self.VERSION, 
+                     'git_url': self.GIT_URL, 'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
